988f19bb1c39f852a41a55fa1ff64a0847fc5c1a
vagrant <vagrant@homestead>
2018-12-20 06:48:56 +0000

    商品模型

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -3,16 +3,17 @@
 namespace App\Admin\Controllers;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
-use Encore\Admin\Controllers\HasResourceActions;
+
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
-use Encore\Admin\Show;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
 
 class UserController extends Controller
 {
-    use HasResourceActions;
+    use ModelForm;
 
     /**
      * Index interface.
@@ -51,27 +52,43 @@ class UserController extends Controller
      */
     protected function grid()
     {
-        $grid = new Grid(new User);
+        // 根据回调函数，在页面上用表格的形式展示用户记录
+        return Admin::grid(User::class, function (Grid $grid) {
 
-        $grid->id('ID')->sortable();
-        $grid->name('用户名');
-        $grid->email('邮箱');
-        $grid->email_verified('已验证邮箱')->display(function ($value) {
-            $value ? '是' : '否';
-        });
-        $grid->created_at('注册时间');
-        $grid->disableCreateButton();
-        $grid->actions(function ($actions) {
-            //不在每行后面展示查看按钮
-            $actions->disableView();
-            $actions->disableDelete();
-            $actions->disableEdit();
-        });
+            // 创建一个列名为 ID 的列，内容是用户的 id 字段，并且可以在前端页面点击排序
+            $grid->id('ID')->sortable();
 
-        $grid->tools(function ($tools) {
-            //禁用批量删除按钮
-            $tools->batch(function ($batch){
-                $batch->disableDelete();
+            // 创建一个列名为 用户名 的列，内容是用户的 name 字段。下面的 email() 和 created_at() 同理
+            $grid->name('用户名');
+
+            $grid->email('邮箱');
+
+            $grid->email_verified('已验证邮箱')->display(function ($value) {
+                return $value ? '是' : '否';
+            });
+
+            $grid->created_at('注册时间');
+
+            // 不在页面显示 `新建` 按钮，因为我们不需要在后台新建用户
+            $grid->disableCreateButton();
+
+            $grid->actions(function ($actions) {
+                // 不在每一行后面展示查看按钮
+                $actions->disableView();
+
+                // 不在每一行后面展示删除按钮
+                $actions->disableDelete();
+
+                // 不在每一行后面展示编辑按钮
+                $actions->disableEdit();
+            });
+
+            $grid->tools(function ($tools) {
+
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
             });
         });
     }
