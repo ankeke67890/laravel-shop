c73695ea3db26d99afe99f0e17be7f76a5c841be
vagrant <vagrant@homestead>
2018-12-29 04:47:26 +0000

    修改databases

--- app/helpers.php
+++ app/helpers.php
@@ -3,14 +3,15 @@
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
-        $url = parse_url(getenv("DATABASE_URL"));
+        //$url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
             'connection' => 'pgsql',
-            'host' => $url["host"],
-            'database'  => substr($url["path"], 1),
-            'username'  => $url["user"],
-            'password'  => $url["pass"],
+            'host' => 'ec2-54-235-169-191.compute-1.amazonaws.com',
+            'port' => '5432',
+            'database'  => 'd7c76j5e3m94br',
+            'username'  => 'zrsuimzepzhzut',
+            'password'  => '83fdb317d24dfb2c5e8662d647ee80e45886fdef24a31ae27d3ead72bd7d2cfc',
         ];
     } else {
         return $db_config = [
