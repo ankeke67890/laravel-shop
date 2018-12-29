85fb1edc5a4a0536448ac0a1a2e7c8f47ce01681
vagrant <vagrant@homestead>
2018-12-29 10:06:06 +0000

    修改数据配置

--- app/helpers.php
+++ app/helpers.php
@@ -7,10 +7,10 @@ function get_db_config()
 
         return $db_config = [
             'connection' => 'pgsql',
-            'host' => 'ec2-54-235-169-191.compute-1.amazonaws.com',
-            'database'  => 'd7c76j5e3m94br',
-            'username'  => 'zrsuimzepzhzut',
-            'password'  => '83fdb317d24dfb2c5e8662d647ee80e45886fdef24a31ae27d3ead72bd7d2cfc',
+            'host' => 'ec2-107-20-237-78.compute-1.amazonaws.com',
+            'database'  => 'd95lbl7qjnjjs3',
+            'username'  => 'hilnqxfzfnfuvj',
+            'password'  => '71815ade882ee92601547d1933c6a1260bb50df517d70dd7f7d6bbc8ce2ba4c9',
         ];
     } else {
         return $db_config = [
