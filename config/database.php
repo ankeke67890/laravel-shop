b834ef3f9dff1e1f1dfc4e0d152755d013d02ace
vagrant <vagrant@homestead>
2018-12-29 04:03:31 +0000

    添加userseeder

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'homestead'),
+            'database' => env('DB_DATABASE', 'laravel-shop'),
             'username' => env('DB_USERNAME', 'homestead'),
             'password' => env('DB_PASSWORD', 'secret'),
             'unix_socket' => env('DB_SOCKET', ''),
